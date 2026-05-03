7938d35581186b625cb39fbf06214b55fc171e22
FiammaMuscari <fiammamuscari@gmail.com>
2026-05-03 03:55:00 -0300

    Fix Linux release asset rebuild workflow

--- packaging/linux/deploy-runtime-libs.sh
+++ packaging/linux/deploy-runtime-libs.sh
@@ -37,7 +37,7 @@ should_exclude_library() {
     libGL.so*|libEGL.so*|libGLX.so*|libOpenGL.so*|libGLdispatch.so*|libvulkan.so*|libdrm.so*|libgbm.so*|libva.so*|libva-drm.so*|libva-x11.so*|libvdpau.so*|libOpenCL.so*|libcuda.so*|libnvidia-*.so*)
       return 0
       ;;
-    libsystemd.so*|libudev.so*|libdbus-1.so*|libselinux.so*|libapparmor.so*|libcap.so*|libmount.so*|libblkid.so*)
+    libsystemd.so*|libudev.so*|libdbus-1.so*|libapparmor.so*|libcap.so*|libmount.so*|libblkid.so*)
       return 0
       ;;
   esac
